b3717278a8c3c8d9b58ff0a69f40fde61584f7eb
Sergio Aragonés <sergioat7@gmail.com>
2019-12-07 22:06:25 +0100

    Loaded XML from URL in DOMParser.

--- DOM/DOM Parser.java
+++ DOM/DOM Parser.java
@@ -1,11 +1,31 @@
 package DOM;
 
+import java.io.IOException;
+import java.net.URL;
+import org.w3c.dom.Document;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+import org.xml.sax.SAXException;
+
 class DOMParser {
 
+    private static final String uri = "https://www.europapress.es/rss/rss.aspx";
 
     public static void main(String[] args) {
 
-        
+        DocumentBuilderFactory dbf = DocumentBuilderFactory.newInstance();
+        DocumentBuilder db;
+        Document doc;
+
+        try {
+            db = dbf.newDocumentBuilder();
+            doc = db.parse(new URL(uri).openStream());
+            doc.getDocumentElement().normalize();
+        } catch (SAXException | IOException | ParserConfigurationException e) {
+            e.printStackTrace();
+        }
     }
 
 }
