af4bcb244fe68f7c073abd82fac60fb5cb45c15c
Sergio Aragonés <sergioat7@gmail.com>
2019-12-12 00:22:36 +0100

    Code refactoring

--- DOM/DOM Parser.java
+++ DOM/DOM Parser.java
@@ -7,6 +7,7 @@ import java.net.URL;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Paths;
+import java.util.List;
 import java.util.stream.Stream;
 
 import org.json.*;
@@ -27,89 +28,105 @@ import javax.xml.transform.stream.StreamResult;
 
 import org.xml.sax.SAXException;
 
+import Model.NewsModel;
+
 class DOMParser {
 
-    private static final String uri = "https://www.europapress.es/rss/rss.aspx";
+    private static String filename = "DOM/noticias_";
+    private static String xmlExtension = ".xml";
+    private static String jsonExtension = ".json";
 
-    public static void main(String[] args) {
+    private NewsModel model;
+
+    public DOMParser() {
+        this.model = new NewsModel();
+    }
+
+    /* Parsear un documento XML para obtener su árbol DOM */
+    private Document obtenerDOM(String fileURL) throws IOException, SAXException, ParserConfigurationException {
 
         DocumentBuilderFactory dbf = DocumentBuilderFactory.newInstance();
-        DocumentBuilder db;
-        Document doc;
 
-        try {
-            db = dbf.newDocumentBuilder();
-            doc = db.parse(new URL(uri).openStream());
-            doc.getDocumentElement().normalize();
+        DocumentBuilder db = dbf.newDocumentBuilder();
 
-            Document outputDoc = db.newDocument();
+        Document doc = db.newDocument();
+        if (!(fileURL.isEmpty()))
+            doc = db.parse(new URL(fileURL).openStream());
+        return doc;
+    }
 
-            Element channelNode = (Element) doc.getElementsByTagName("channel").item(0);
-            String channelTitle = channelNode.getElementsByTagName("title").item(0).getTextContent();
+    /* Mostrar contenido de un documento XML a través de su árbol DOM */
+    private void mostrarDOM(Document doc) {
 
-            Element channelNews = outputDoc.createElement("noticias");
-            channelNews.setAttribute("canal", channelTitle);
-            outputDoc.appendChild(channelNews);
-            System.out.println("Titulo canal:" + channelTitle);
-            System.out.println("URL canal:" + channelNode.getElementsByTagName("link").item(0).getTextContent());
-            System.out.println(
-                    "Descripcion canal:" + channelNode.getElementsByTagName("description").item(0).getTextContent());
+        Element channelNode = (Element) doc.getElementsByTagName("channel").item(0);
+        String channelTitle = getTextNodeContent(channelNode, "title");
 
-            NodeList items = channelNode.getElementsByTagName("item");
-            Node currentNode;
-            Element currentItem;
-            for (int i = 0; i < items.getLength(); i++) {
+        System.out.println(
+                " - Titulo canal:" + channelTitle + "\n" +
+                " - URL canal:" + getTextNodeContent(channelNode, "link") + "\n" +
+                " - Descripcion canal:" + getTextNodeContent(channelNode, "description"));
 
-                currentNode = items.item(i);
+        this.model.setTitle(channelTitle);
 
-                if (currentNode.getNodeType() == Node.ELEMENT_NODE) {
-                    currentItem = (Element) items.item(i);
-                    int id = i + 1;
-                    String newsTitle = currentItem.getElementsByTagName("title").item(0).getTextContent();
+        NodeList items = channelNode.getElementsByTagName("item");
+        Node currentNode;
+        Element currentItem;
+        for (int i = 0; i < items.getLength(); i++) {
 
-                    Element news = outputDoc.createElement("noticia");
-                    channelNews.appendChild(news);
-                    news.appendChild(outputDoc.createTextNode(newsTitle));
+            currentNode = items.item(i);
 
-                    System.out.println("Noticia " + id);
-                    System.out.println("   Titulo:" + newsTitle);
-                    System.out.println("   URL:" + currentItem.getElementsByTagName("link").item(0).getTextContent());
-                    System.out.println("   Descripcion:"
-                            + currentItem.getElementsByTagName("description").item(0).getTextContent());
-                    System.out.println("   Fecha de publicacion:"
-                            + currentItem.getElementsByTagName("pubDate").item(0).getTextContent());
-                    System.out.println(
-                            "   Categoria:" + currentItem.getElementsByTagName("category").item(0).getTextContent());
-                }
+            if (currentNode.getNodeType() == Node.ELEMENT_NODE) {
+
+                currentItem = (Element) items.item(i);
+                int id = i + 1;
+                String newsTitle = getTextNodeContent(currentItem, "title");
+
+                System.out.println(" - Noticia " + id + "\n" +
+                                    "   * Titulo:" + newsTitle + "\n" +
+                                    "   * URL:"  + getTextNodeContent(currentItem, "link") + "\n" +
+                                    "   * Descripcion:" + getTextNodeContent(currentItem, "description") + "\n" +
+                                    "   * Fecha de publicacion:" + getTextNodeContent(currentItem, "pubDate") + "\n" +
+                                    "   * Categoria:" + getTextNodeContent(currentItem, "category"));
+
+                this.model.addNews(newsTitle);
             }
-
-            generateXMLFile(outputDoc, "DOM/noticias_" + channelTitle + ".xml");
-            getXMLfromJson("DOM/noticias_" + channelTitle);
-        } catch (SAXException | IOException | ParserConfigurationException e) {
-            e.printStackTrace();
-        }
-
-    }
-
-    private static void generateXMLFile(Document doc, String filename) {
-
-        try {
-            TransformerFactory tf = TransformerFactory.newInstance();
-            Transformer transformer = tf.newTransformer();
-            transformer.setOutputProperty(OutputKeys.VERSION, "1.0");
-            transformer.setOutputProperty(OutputKeys.ENCODING, "UTF-8");
-            transformer.setOutputProperty(OutputKeys.STANDALONE, "yes");
-            transformer.setOutputProperty(OutputKeys.INDENT, "yes");
-            transformer.transform(new DOMSource(doc), new StreamResult(new File(filename)));
-        } catch (TransformerException e) {
-            e.printStackTrace();
         }
     }
 
-    private static void getXMLfromJson(String filename) {
+    /* Generar un documento XML a partir de un árbol DOM */
+    private void generateXMLFile()
+            throws TransformerException, IOException, SAXException, ParserConfigurationException {
 
+        Document doc = this.obtenerDOM("");
+
+        Element channelNews = doc.createElement("noticias");
+        String channelTitle = this.model.getTitle();
+        channelNews.setAttribute("canal", channelTitle);
+        doc.appendChild(channelNews);
+
+        List<String> news = this.model.getNews();
+        for (int i = 0; i < news.size(); i++) {
+            Element singleNews = doc.createElement("noticia");
+            channelNews.appendChild(singleNews);
+            singleNews.appendChild(doc.createTextNode(news.get(i)));
+        }
+
+        TransformerFactory tf = TransformerFactory.newInstance();
+        Transformer transformer = tf.newTransformer();
+        transformer.setOutputProperty(OutputKeys.VERSION, "1.0");
+        transformer.setOutputProperty(OutputKeys.ENCODING, "UTF-8");
+        transformer.setOutputProperty(OutputKeys.STANDALONE, "yes");
+        transformer.setOutputProperty(OutputKeys.INDENT, "yes");
+        transformer.transform(new DOMSource(doc), new StreamResult(new File(filename + channelTitle + xmlExtension)));
+    }
+
+    /* Generar un documento JSON a partir de un documento XML */
+    private void generateJSONFileFromXMLFile() {
+
+        String channelTitle = this.model.getTitle();
         StringBuilder contentBuilder = new StringBuilder();
-        try (Stream<String> stream = Files.lines(Paths.get(filename + ".xml"), StandardCharsets.UTF_8)) {
+        try (Stream<String> stream = Files.lines(Paths.get(filename + channelTitle + xmlExtension),
+                StandardCharsets.UTF_8)) {
             stream.forEach(s -> contentBuilder.append(s).append("\n"));
         } catch (IOException e) {
             e.printStackTrace();
@@ -119,7 +136,7 @@ class DOMParser {
         try {
             JSONObject xmlJSONObj = JSONML.toJSONObject(XML);
 
-            try (FileWriter file = new FileWriter(filename + ".json")) {
+            try (FileWriter file = new FileWriter(filename + channelTitle + jsonExtension)) {
 
                 file.write(xmlJSONObj.toString());
                 file.flush();
@@ -132,4 +149,27 @@ class DOMParser {
         }
     }
 
+    // Obtener el contenido de un nodo de texto */
+    private static String getTextNodeContent(Element elem, String tag) {
+        return elem.getElementsByTagName(tag).item(0).getTextContent();
+    }
+
+
+
+    public static void main(String[] args) {
+
+        try {
+            DOMParser parser = new DOMParser();
+
+            Document doc = parser.obtenerDOM("https://www.europapress.es/rss/rss.aspx");
+
+            parser.mostrarDOM(doc);
+
+            parser.generateXMLFile();
+            parser.generateJSONFileFromXMLFile();
+        } catch (SAXException | IOException | ParserConfigurationException | TransformerException e) {
+            e.printStackTrace();
+        }
+
+    }
 }
